923f174d4b20372d4e4872191802e09396d8fe53
Tuan-Simonetti <tuan.gomes@picpay.com>
2020-08-07 17:36:51 -0300

    Repositorio paralelo de estudos

---
+++ src/classesAndMetodos/classeVsObjeto/metodos/Metodos.java
@@ -0,0 +1,20 @@
+package classesAndMetodos.classeVsObjeto.metodos;
+
+import java.util.Scanner;
+import java.util.zip.DeflaterOutputStream;
+
+public class Metodos {
+
+    String nome;
+    double preco;
+    double desconto;
+
+
+    double precoComDesconto() {
+        return preco * (1 - desconto);
+    }
+
+    double precoComDesconto(double descontoDoGerente) {
+        return preco * (1 - desconto + descontoDoGerente);
+    }
+}
---
+++ src/classesAndMetodos/classeVsObjeto/metodos/desafioMetodos/BaseDatas.java
@@ -0,0 +1,11 @@
+package classesAndMetodos.classeVsObjeto.metodos.desafioMetodos;
+
+import java.util.Date;
+
+public class BaseDatas {
+
+    String dia;
+    String mes;
+    String ano;
+    Date data;
+}
