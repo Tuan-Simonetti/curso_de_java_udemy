b4778986f7445f63c96e8ddf804841dfe2136ebc
Tuan-Simonetti <tuan.gomes@picpay.com>
2020-08-26 17:02:12 -0300

    Inicio dos estudos em Programacao Orientada ao Objeto

---
+++ src/programacaoOrientadaObjeto/composicaoRelacao1para1/Carro.java
@@ -0,0 +1,33 @@
+package programacaoOrientadaObjeto.composicaoRelacao1para1;
+
+public class Carro {
+
+    Motor motor;
+
+    Carro() {
+        this.motor = new Motor(this);
+    }
+
+//    Motor motor = new Motor();
+    void acelerar() {
+        motor.fatorInjecao += 0.4;
+    }
+
+    void frear() {
+        if (motor.fatorInjecao < 2.6) {
+            motor.fatorInjecao -= 0.4;
+        }
+    }
+
+    void ligar() {
+        motor.ligado = true;
+    }
+
+    void desligar() {
+        motor.ligado = false;
+    }
+
+    boolean estaLigado() {
+        return motor.ligado;
+    }
+}
